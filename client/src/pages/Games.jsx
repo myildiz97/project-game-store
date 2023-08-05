f33781fa959e92a36ab2ba34e59e9b854a53dd6c
Mehmet Yıldız <myildizwork@gmail.com>
2023-08-05 16:55:57 +0300

    search-bar added & css updated

--- client/src/pages/Games.jsx
+++ client/src/pages/Games.jsx
@@ -1,44 +1,62 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Loader from "../tools/Loader.jsx";
+import SearchBar from "../tools/SearchBar.jsx";
+
+export const GameContext = createContext();
 
 const Games = () => {
   const [games, setGames] = useState([]);
+  const [defGames, setDefGames] = useState([]);
+  const [notFound, setNotFound] = useState(false);
 
   useEffect(() => {
     axios.get("https://project-game.onrender.com/games")
-      .then((res) => setGames(res.data))
+      .then((res) => {
+        setGames(res.data);
+        setDefGames(res.data);
+      })
       .catch((error) => console.error("Error fetching games:", error));
   }, []);
   
   return (
-    <div className="games">
-      <h1>Unmissable Gaming Gems: The Best Selection of Must Play Games</h1>
-      <hr />
-      {games.length > 0 ? (
-        <div className="games-wrapper">
-          {games?.map((game) => {
-            const { id, title, imgUrl, categories } = game;
-            return (
-              <div key={id} className="game">
-                <img src={imgUrl} alt={title} />
-                <h3>{title}</h3>
-                <div>
-                  <span>Categories: </span>
-                  <span>{categories.join(", ")}</span>
+    <GameContext.Provider value={{ defGames, setGames, setNotFound }}>
+      <div className="games">
+        <h1>Unmissable Gaming Gems: The Best Selection of Must Play Games</h1>
+        <hr />
+        <SearchBar />
+        {
+          notFound && (
+            <div className="notFound">
+                <p>There is no game for the search result...</p>
+            </div>
+          )
+        }
+        {!notFound && games?.length > 0 ? (
+          <div className="games-wrapper">
+            {games?.map((game) => {
+              const { id, title, imgUrl, categories } = game;
+              return (
+                <div key={id} className="game">
+                  <img src={imgUrl} alt={title} />
+                  <h3>{title}</h3>
+                  <div>
+                    <span>Categories: </span>
+                    <span>{categories.join(", ")}</span>
+                  </div>
+                  <Link to={`/games/${id}`}>See more details...</Link>
+                  {id !== games.length && <hr />}
                 </div>
-                <Link to={`/games/${id}`}>See more details...</Link>
-                {id !== games.length && <hr />}
-              </div>
-            )
+              )
             })}
-        </div>
-      ): (
-        <Loader />
-      )}
-    </div>
+          </div>
+        ): (
+          !notFound && <Loader />
+          )}
+      </div>
+    </GameContext.Provider>
   );
-}
+};
 
-export default Games
+export default Games;
---
+++ client/src/tools/SearchBar.jsx
@@ -0,0 +1,40 @@
+import { useContext, useState } from "react";
+import { GameContext } from "../pages/Games.jsx";
+import { LiaSearchSolid } from "react-icons/lia";
+
+const SearchBar = () => {
+  const { defGames, setGames, setNotFound } = useContext(GameContext);
+  const [search, setSearch] = useState("");
+
+  const filterGames = (games, search) => games.filter(game => game.title.toLowerCase().match(search));
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    let value = e.target.value.toLowerCase();
+    setSearch(value);
+    if (value.length > 0) {
+      const filtered = filterGames(defGames, value);
+      filtered.length > 0 ? setGames(filtered) : setNotFound(true);
+    } else {
+      setGames(defGames);
+      setNotFound(false);
+    };
+  };
+
+  return (
+    <div className="search-bar">
+      <label htmlFor="search-bar-input">
+        <LiaSearchSolid size={40} color="#d90429"/>
+      </label>
+      <input
+        id="search-bar-input"
+        type="text"
+        placeholder="Red Dead Redemption 2"
+        onChange={handleChange}
+        value={search}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
